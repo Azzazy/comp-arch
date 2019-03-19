a28876995cc4efe0632192bee98db9119a7bc1ad
unknown <msa.azzazy>
2019-03-19 04:30:29 +0200

    finished

--- single_cycle.srcs/sources_1/new/DataMem.v
+++ single_cycle.srcs/sources_1/new/DataMem.v
@@ -9,5 +9,10 @@ module DataMem(input clk, input MemRead, input MemWrite,input [5:0] addr, input 
         mem[0]=32'd17;
         mem[1]=32'd9;
         mem[2]=32'd25;
+        
+//        mem[0]=32'd0; //final sum
+//        mem[1]=32'd5; //increment value
+//        mem[2]=32'd3; //number of steps
+//        mem[3]=32'd1; //step
     end
 endmodule
